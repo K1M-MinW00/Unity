30751d3c1122c9d643aab4adb2d6bde9de8c4f78
김민우 <122630746+K1M-MinW00@users.noreply.github.com>
2024-03-26 18:26:45 +0900

    Create Card.cs

---
+++ 스파르타 코딩클럽/게임 개발 종합반/5주차/Card.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Card : MonoBehaviour
+{
+    public SpriteRenderer frontImage; // SpriteRenderer 컴포넌트를 받을 변수 frontImage (카드의 그림 이미지)
+
+    public GameObject front; // 카드의 앞면 front 오브젝트
+    public GameObject back; // 카드의 뒷면 back 오브젝트
+
+    public Animator anim; // 카드의 animator 컴포넌트를 받을 변수 anim 
+
+    AudioSource audioSource; // 카드의 AudioSource 컴포넌트를 받을 변수 audioSource
+    public AudioClip clip; // 카드 뒤집을 때 재생될 AudioSource 에 넣어줄 AudioClip
+
+    public int idx = 0; // 카드의 그림 정보를 저장할 변수 idx
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();  // Card 오브젝트에 붙어 있는 AudioSource 컴포넌트 사용
+    }
+
+    public void Setting(int num)
+    {
+        idx = num; // Board.cs 에서 전달해준 num 을 Card 에 저장
+        frontImage.sprite = Resources.Load<Sprite>($"rtan{idx}"); // Resources 폴더에 있는 rtan 이미지를 가져오기
+    }
+
+    public void OpenCard()
+    {
+
+        audioSource.PlayOneShot(clip); // clip 을 한번만 재생
+
+        anim.SetBool("isOpen", true);
+        front.SetActive(true);
+        back.SetActive(false);
+
+        if (GameManager.instance.firstCard == null) // firstCard 가 비었다면
+        {
+            GameManager.instance.firstCard = this; // firstCard 에 카드 정보를 넘겨준다.
+        }
+        else //secondCard 가 비었다면
+        {
+            GameManager.instance.secondCard = this; // secondCard 에 카드 정보를 넘겨준다.
+            GameManager.instance.Matched(); // Matched() 함수를 호출
+        }
+    }
+
+    public void DestroyCard()
+    {
+        // DestroycardInvoke 함수를 1초 뒤에 실행
+        Invoke("DestroyCardInvoke", 1.0f);
+    }
+
+    void DestroyCardInvoke()
+    {
+        // card 게임 오브젝트 삭제
+        Destroy(gameObject);
+    }
+
+    public void CloseCard()
+    {
+        // CloseCardInvoke 함수를 1초 뒤에 실행
+        Invoke("CloseCardInvoke", 1.0f);
+    }
+
+    void CloseCardInvoke()
+    {
+        anim.SetBool("isOpen", false);
+        front.SetActive(false);
+        back.SetActive(true);
+    }
+}
